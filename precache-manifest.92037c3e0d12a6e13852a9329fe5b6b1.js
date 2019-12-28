707684eaa8e49b40b0ec337f64456e593da2a36b
xujustinj <xu.justin.j@gmail.com>
2019-12-27 19:25:42 -0500

    Updates

---
+++ precache-manifest.92037c3e0d12a6e13852a9329fe5b6b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfe43010ada4a4b421b11e2f69c2c7e",
+    "url": "/Asteria/index.html"
+  },
+  {
+    "revision": "7e359bca6513ad606752",
+    "url": "/Asteria/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8dc8de060736bdf735b3",
+    "url": "/Asteria/static/js/2.e9beab60.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Asteria/static/js/2.e9beab60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e359bca6513ad606752",
+    "url": "/Asteria/static/js/main.118a1d2d.chunk.js"
+  },
+  {
+    "revision": "d636057232d7c12390b7",
+    "url": "/Asteria/static/js/runtime-main.21b4b72c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Asteria/static/media/logo.5d5d9eef.svg"
+  }
+]);
