dc47859f09def294c57497ed75749fcd58f27887
xujustinj <xu.justin.j@gmail.com>
2020-02-03 23:17:00 -0500

    Make the home page prettier with Accordions

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import {
 import './stylesheets/adventure.css';
 import './stylesheets/layout.css';
 import githubImage from './img/github-logo-and-text.png';
+
+import Welcome from './Welcome';
 import Asteria42 from "./asteria42/Asteria42";
 import AsteriaXOR from "./asteriaXOR/AsteriaXOR";
 import AsteriaNAND from "./asteriaNAND/AsteriaNAND";
@@ -101,38 +103,7 @@ const Leftbar = ({ visible, routes }: LeftbarProps) => (
     </Sidebar>
 )
 
-function Welcome() {
-    return (
-      <div>
-        <h3>What</h3>
-        <p>Asteria is the product of a ongoing adventure into the world of machine learning. The goal: to have a tool that can identify and translate <a href="https://madeon.fandom.com/wiki/Adventure_alphabet">Madeon's Adventure alphabet</a> on the fly (hence the name Asteria). By no means is Asteria anywhere near that goal, nor am I making a direct beeline for it. I'm taking a winding path that will hopefully maximize the amount I can learn from this project.</p>
-        <p>In the sidebar are 3 small-scale demonstrations of Asteria's learning power that probably won't melt your CPUs. Enjoy!</p>
-
-        <h3>How</h3>
-        <p>So far, I have:</p>
-        <ul>
-          <li>built an <b>object-oriented multilayer perceptron</b> library</li>
-          <li>implemented <b>symbolic differentiation</b> (deprecated) in <b>TypeScript</b></li>
-          <li>re-derived the matrix shortcuts for computing <b>backpropagation</b> and implemented them in <b>TypeScript</b></li>
-          <li>added <b>momentum</b> and <b>orthogonal initialization</b> to speed up learning</li>
-          <li>built this website with <b>React</b> and <b>React Router</b></li>
-          <li>re-learned <b>CSS</b> via <b>SASS (scss)</b></li>
-        </ul>
-
-        <h3>Who</h3>
-        <p>Special thanks to:</p>
-        <ul>
-          <li><b>Grant Sanderson (3Blue1Brown)</b> for making the <a href="https://www.youtube.com/playlist?list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi">videos</a> that kicked off this adventure</li>
-          <li><b>TwoNineFive</b> for the putting together the <a href="https://fontmeme.com/fonts/madeon-runes-font/">lovely font</a> that's dotted around this site</li>
-        </ul>
-
-        <h3>Why</h3>
-        <p>If you're wondering why I implemented machine learning in JavaScript... why not?</p>
-      </div>
-    )
-}
-
-function App() {
+const App = () => {
     const [leftbar, setLeftbar] = useState(false);
 
     return (
