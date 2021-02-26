1f280e744bfcb00ef62ae1130a5df4a4b2b64ab3
Justin Xu <xu.justin.j@gmail.com>
2021-02-26 01:41:47 -0500

    Burn all and start anew

--- client/src/deprecated/diffable/differentiable.ts
+++
@@ -1,9 +0,0 @@
-import Variable from './variable';
-
-interface Differentiable {
-    value(): number
-    deriv(_v: Variable): number
-    // print(): string
-}
-
-export default Differentiable;
--- client/src/deprecated/neuro/trainable.ts
+++
@@ -1,8 +0,0 @@
-import { Differentiable } from "../diffable";
-
-interface Trainable {
-    study(error: Differentiable): void;
-    learn(sensitivity: number, friction: number): void;
-}
-
-export default Trainable;
